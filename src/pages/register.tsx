b038f960e9a1e30f4b8264bbedd6bcc4b8549552
Jish2 <j.j.jappiecraft@gmail.com>
2024-01-27 12:10:51 -0800

    add supabase structure

---
+++ src/pages/register.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import type { Session } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+type Props = {};
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "",
+);
+
+const RegisterPage = (props: Props) => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => {
+        setSession(session);
+      })
+      .catch((error) => console.error(error));
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <div className="h-full w-full">
+      (!session) ? (
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />) : (
+      <div>Logged in!</div>)
+    </div>
+  );
+};
+
+export default RegisterPage;
