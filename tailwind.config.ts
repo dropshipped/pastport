7e71d4a15dfff1a20857512646b09cf7aae72641
Jish2 <j.j.jappiecraft@gmail.com>
2024-01-26 23:33:42 -0800

    add next ui and themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,22 @@
+import { nextui } from "@nextui-org/react";
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: [
+          // "var(--font-sans)",
+          ...fontFamily.sans,
+        ],
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
