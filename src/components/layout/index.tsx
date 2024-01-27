1241c0169fc18a6ee8d2002c4880de0e09b693a9
Jish2 <j.j.jappiecraft@gmail.com>
2024-01-27 12:10:58 -0800

    add layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  <div className="relative flex h-full w-full items-center justify-center">
+    {children}
+  </div>;
+};
+
+export default Layout;
