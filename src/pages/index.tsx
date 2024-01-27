2cc851c5e5a5e023b9d6848e2a888b624542555e
Andrew Ho <andyminhtuanho@gmail.com>
2024-01-26 19:19:47 -0800

    Added supabase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import { env } from "~/env";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
