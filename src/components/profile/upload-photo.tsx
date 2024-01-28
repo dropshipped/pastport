8767119dc849ecd5cff0c2bd7a64c3356a34dea4
Andrew Ho <andyminhtuanho@gmail.com>
2024-01-27 23:48:06 -0800

    image location extraction

--- src/components/profile/upload-photo.tsx
+++ src/components/profile/upload-photo.tsx
@@ -3,60 +3,107 @@
  * @see https://v0.dev/t/Vrn2uEHO0ir
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import { CardHeader, CardBody, Card, Input } from "@nextui-org/react";
-import { randomUUID } from "crypto";
-import { type ChangeEvent, useState } from "react";
-import { supabase } from "~/components/providers/auth";
+import { CardHeader, CardBody, Card, Input, Spinner } from "@nextui-org/react";
+// import { randomUUID } from "crypto";
+import { type ChangeEvent, useState, useEffect } from "react";
+// import { supabase } from "~/components/providers/auth";
 
-import ExifReader from "exifreader";
+// import ExifReader from "exifreader";
+// import EXIF from "exif-js";
+import { parse } from "exifr";
 
 export function UploadPhotoButton() {
-  const [image, setImage] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
 
-  if (image) {
-    const tags = ExifReader.load(image!);
-    console.log(tags);
+  // function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  //   setFile(e?.target?.files![0]!);
+  // }
+
+  useEffect(() => {
+    handleExtract();
+  }, [file]);
+
+  // const getData = async () => {
+  //   if (file) {
+  //     // const tags = ExifReader.load(image!);
+  //     // console.log(tags);
+  //     // console.log(await EXIF.getAllTags(image));
+  //     console.log("ac parsing image metadata");
+  //     const res = await exifr.gps(file);
+  //     // console.log();
+  //   }
+  // };
+
+  async function handleExtract() {
+    if (!file) return;
+    setLoading(true);
+    const data = await parse(file as File, true);
+    console.log(data);
+    setLoading(false);
   }
 
+  /*
   const onSubmit = async () => {
-    if (image === null) {
+    if (file === null) {
       console.log("null image");
       return;
     }
 
     const { data, error } = await supabase.storage
       .from("avatars")
-      .upload(`public/${randomUUID()}.png`, image, {
+      .upload(`public/${randomUUID()}.png`, file, {
         cacheControl: "3600",
         upsert: false,
       });
 
     console.log(data, error);
   };
+  */
 
   return (
     <Card className="flex w-full max-w-md items-center justify-center border-2 border-dashed border-gray-300 p-10 shadow-sm dark:border-gray-700">
-      <label className="cursor-pointer" htmlFor="file-upload">
-        <CardBody className="flex flex-col items-center justify-center space-y-4">
-          <CardHeader className="flex flex-col items-center justify-center space-y-4">
-            <UploadIcon className="h-12 w-12 text-gray-500 dark:text-gray-400" />
-            <h1 className="dark:text-gray flex items-center text-center text-lg text-gray-500 no-underline">
-              Tap Here to Upload a Photo
-            </h1>
-          </CardHeader>
-          <Input
-            className="hidden"
-            id="file-upload"
-            type="file"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              if (e.target.files && e.target.files.length > 0) {
-                const image = e.target.files[0]!; // Safely access the file using TypeScript
-                setImage(image); // Set the file state
-              }
-            }}
-          />
-        </CardBody>
-      </label>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <label className="cursor-pointer" htmlFor="file-upload">
+          <CardBody className="flex flex-col items-center justify-center space-y-4">
+            <CardHeader className="flex flex-col items-center justify-center space-y-4">
+              <UploadIcon className="h-12 w-12 text-gray-500 dark:text-gray-400" />
+              <h1 className="dark:text-gray flex items-center text-center text-lg text-gray-500 no-underline">
+                Tap Here to Upload a Photo
+              </h1>
+            </CardHeader>
+            <input
+              className="hidden"
+              id="file-upload"
+              type="file"
+              onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+                // const image = e.target.files[0]!; // Safely access the file using TypeScript
+                setFile(e?.target?.files![0]!);
+                // if (e.target.files && e.target.files.length > 0) {
+                //   const image = e.target.files[0]!; // Safely access the file using TypeScript
+                //   setFile(image); // Set the file state
+                // }
+                // setLoading(true);
+                // console.log("loading", loading);
+                // console.log("started parsing image metadata");
+                // await new Promise(() =>
+                //   setTimeout(() => {
+                //     console.log("Finished");
+                //     1;
+                //   }, 1000),
+                // );
+                // await handleExtract();
+                // console.log("stopped parsing image metadata");
+
+                // setLoading(false);
+                // console.log("not loading", loading);
+              }}
+            />
+          </CardBody>
+        </label>
+      )}
     </Card>
   );
 }
